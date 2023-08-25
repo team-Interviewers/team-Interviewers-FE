14437d655559cfad2301c3011c8e0e14bb6a5d2d
ww8007 <wshmin1234@gmail.com>
2023-08-25 14:38:59 +0900

    fix: shadow-root 방식에서 일반 portal 방식으로 변경

--- src/pages/content/components/Portal/index.tsx
+++ src/pages/content/components/Portal/index.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import Portal from '@root/src/pages/content/components/Portal/Portal';
 import refreshOnUpdate from 'virtual:reload-on-update-in-view';
-import { attachTwindStyle } from '@src/shared/style/twind';
 
 refreshOnUpdate('pages/content');
 
@@ -10,12 +9,4 @@ const portalDiv = document.createElement('div');
 portalDiv.id = 'portal';
 document.body.insertBefore(portalDiv, document.body.firstChild);
 
-const rootIntoShadow = document.createElement('div');
-rootIntoShadow.id = 'shadow-root';
-
-const shadowRoot = portalDiv.attachShadow({ mode: 'open' });
-shadowRoot.appendChild(rootIntoShadow);
-
-attachTwindStyle(rootIntoShadow, shadowRoot);
-
-createRoot(rootIntoShadow).render(<Portal />);
+createRoot(portalDiv).render(<Portal />);
