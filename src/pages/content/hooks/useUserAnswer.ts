098aaac140756c394d070d4e118f4874eb0a515f
ww8007 <wshmin1234@gmail.com>
2023-08-25 14:42:56 +0900

    feat: 사용자의 답을 관리하는 useUserAnswer custom hook 추가

---
+++ src/pages/content/hooks/useUserAnswer.ts
@@ -0,0 +1,43 @@
+import { ChangeEvent, useState } from 'react';
+import { Question } from '../types/question';
+
+interface UseUserAnswerReturn {
+  isCorrect: boolean;
+  answer: string;
+  handleChange: (
+    e: ChangeEvent<HTMLTextAreaElement> | ChangeEvent<HTMLInputElement>
+  ) => void;
+}
+
+interface UseUserAnswerProps {
+  question: Question | undefined;
+}
+
+/**
+ * 사용자의 답을 관리하는 커스텀 훅
+ * @param question 문제
+ * @example
+ * const { answer, isCorrect, handleChange } = useUserAnswer(question);
+ */
+
+const useUserAnswer = ({
+  question,
+}: UseUserAnswerProps): UseUserAnswerReturn => {
+  const [answer, setAnswer] = useState('');
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    setAnswer(e.target.value);
+  };
+
+  const isCorrect = answer === question?.correct;
+
+  return {
+    answer,
+    isCorrect,
+    handleChange,
+  };
+};
+
+export default useUserAnswer;
