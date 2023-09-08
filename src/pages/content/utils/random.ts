44a62238eff92ceb1ba56ede0c9a189b3d11359f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-09-08 23:26:39 +0900

    feat: getFilteredQuestion 유틸 추가

--- src/pages/content/utils/random.ts
+++
@@ -1,6 +0,0 @@
-import { Question, Questions } from '../types/question';
-
-export const getRandomQuestion = (questions: Questions): Question => {
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  return questions[randomIndex];
-};
