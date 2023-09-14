08d79c4e689e0ca41db65356edbd2309c0825069
ww8007 <wshmin1234@gmail.com>
2023-09-14 21:46:28 +0900

    feat: 모달 열리는 조건을 담당하는 useTrigger custom hook 추가

--- src/pages/content/components/Portal/Portal.tsx
+++ src/pages/content/components/Portal/Portal.tsx
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { useModal } from '../../hooks/useModal';
+
 import { darkTheme } from '../../utils/theme';
 import ContentLayer from '../ContentLayer';
+import ToastList from '@root/src/shared/ui/toast/ToastList';
+import useTrigger from '../../hooks/useTrigger';
+
+const SETTING_TIME = 3;
 
 export default function Portal() {
-  // NOTE : 일단 편의를 위해서 모달이 계속 열려있도록 하였습니다.
-  const { isOpen, Modal, openModal, closeModal } = useModal();
-  useEffect(() => {
-    openModal();
-  }, []);
+  const { Modal, isOpen, closeModal } = useTrigger({ time: SETTING_TIME });
 
   return (
     <>
       <ThemeProvider theme={darkTheme}>
+        <ToastList />
         <Modal isOpen={isOpen} closeModal={closeModal}>
           <ContentLayer closeModal={closeModal} />
         </Modal>
