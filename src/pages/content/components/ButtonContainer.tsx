17c90be8419cd6e777ac88030841b21ab0a715db
ww8007 <wshmin1234@gmail.com>
2023-09-14 21:42:39 +0900

    design: button reset css 적용

--- src/pages/content/components/ButtonContainer.tsx
+++ src/pages/content/components/ButtonContainer.tsx
@@ -16,4 +16,21 @@ const Button = styled.button`
   align-items: center;
   justify-content: center;
   padding: 3px;
+  border: none;
+  margin: 0;
+  padding: 0;
+  width: auto;
+  overflow: visible;
+
+  background: transparent;
+
+  color: inherit;
+  font: inherit;
+
+  line-height: normal;
+
+  -webkit-font-smoothing: inherit;
+  -moz-osx-font-smoothing: inherit;
+
+  -webkit-appearance: none;
 `;
