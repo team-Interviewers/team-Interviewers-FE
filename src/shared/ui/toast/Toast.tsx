58d40385e20e376876409ddc6326d36b3f3b1c01
ww8007 <wshmin1234@gmail.com>
2023-09-14 21:48:15 +0900

    feat: 전역에서 사용 가능한 Toast ui 컴포넌트 추가

---
+++ src/shared/ui/toast/Toast.tsx
@@ -0,0 +1,97 @@
+import styled, { keyframes } from 'styled-components';
+import useToastStore, { ToastMessage, ToastMode } from './store/toast';
+import { BsFillCheckCircleFill as SuccessIcon } from 'react-icons/bs';
+import { AiOutlineCloseCircle as CloseIcon } from 'react-icons/ai';
+import { BsFillExclamationTriangleFill as ErrorIcon } from 'react-icons/bs';
+import {
+  ColorType,
+  ToastColorType,
+  darkTheme,
+} from '@root/src/pages/content/utils/theme';
+import getRem from '../../util/getRem';
+
+interface ToastProps {
+  message: ToastMessage;
+  mode?: ToastMode;
+}
+
+const icon: Record<ToastMode, React.ReactNode> = {
+  SUCCESS: <SuccessIcon color={darkTheme.colors.white} />,
+  DELETE: <CloseIcon color={darkTheme.colors.white} />,
+  ERROR: <ErrorIcon color={darkTheme.colors.black} />,
+} as const;
+
+const modeColor: Record<ToastMode, ToastColorType> = {
+  SUCCESS: 'success',
+  DELETE: 'delete',
+  ERROR: 'error',
+} as const;
+
+const modeTextColor: Record<ToastMode, ColorType> = {
+  SUCCESS: 'default',
+  DELETE: 'white',
+  ERROR: 'black',
+} as const;
+
+const Toast = ({ message, mode = 'SUCCESS' }: ToastProps) => {
+  const { duration } = useToastStore();
+  return (
+    <StyleToast duration={duration} backgroundColor={modeColor[mode]}>
+      <StyleToastMessage mode={mode}>
+        <Top>
+          {icon[mode]}
+          <ToastText mode={mode}>{message}</ToastText>
+        </Top>
+      </StyleToastMessage>
+    </StyleToast>
+  );
+};
+
+export default Toast;
+
+const floating = keyframes`
+    0% {opacity: 0}
+    25% {opacity: 1}
+    50% {opacity: 1}
+    75% {opacity: 1}
+    100% {opacity: 0}
+`;
+
+interface StyleToastProps {
+  duration: number;
+  backgroundColor: ToastColorType;
+}
+
+const StyleToast = styled.div<StyleToastProps>`
+  position: fixed;
+  bottom: 50%;
+  right: 0;
+  width: ${getRem(300)};
+  left: calc(50% - ${getRem(150)});
+  animation: ${floating} ${({ duration }) => duration}ms ease-in-out;
+  border-radius: ${getRem(8)};
+  background-color: ${(props) => props.theme.toast[props.backgroundColor]};
+`;
+
+const StyleToastMessage = styled.div<{ mode: ToastMode }>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  height: ${getRem(52)};
+  padding: 0 ${getRem(20)};
+  border-radius: ${getRem(8)};
+  background: ${(props) => props.theme.toast[props.mode]};
+`;
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: ${getRem(10)};
+`;
+
+const ToastText = styled.span<{ mode: ToastMode }>`
+  color: ${({ mode }) => modeTextColor[mode]};
+  font-size: ${getRem(14)};
+  font-weight: 500;
+  line-height: ${getRem(20)};
+`;
---
+++ src/shared/ui/toast/ToastList.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+import Toast from './Toast';
+import useToastStore from './store/toast';
+import styled from 'styled-components';
+
+const ModalPortals = ({ children }: { children: ReactNode }) => {
+  const modalDiv = document.createElement('div');
+  modalDiv.id = 'modal';
+  document.body.insertBefore(modalDiv, document.body.firstChild);
+  return createPortal(children, modalDiv);
+};
+
+const ToastList = () => {
+  const { toasts } = useToastStore();
+
+  if (!toasts.length) {
+    return <></>;
+  }
+
+  return (
+    <ModalPortals>
+      <Layout>
+        {toasts.map((toast) => (
+          <Toast key={toast.id} message={toast.message} mode={toast.mode} />
+        ))}
+      </Layout>
+    </ModalPortals>
+  );
+};
+
+export default ToastList;
+
+const Layout = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  z-index: 30;
+`;
