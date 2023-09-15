b5bca58eeb881e9c4a8838342cb9a394fabefafd
ww8007 <wshmin1234@gmail.com>
2023-09-15 20:56:14 +0900

    feat: React Query setting 추가

--- src/pages/content/components/Portal/Portal.tsx
+++ src/pages/content/components/Portal/Portal.tsx
@@ -4,20 +4,22 @@ import { darkTheme } from '../../utils/theme';
 import ContentLayer from '../ContentLayer';
 import ToastList from '@root/src/shared/ui/toast/ToastList';
 import useTrigger from '../../hooks/useTrigger';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const SETTING_TIME = 3;
 
 export default function Portal() {
+  const queryClient = new QueryClient();
   const { Modal, isOpen, closeModal } = useTrigger({ time: SETTING_TIME });
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={darkTheme}>
         <ToastList />
         <Modal isOpen={isOpen} closeModal={closeModal}>
           <ContentLayer closeModal={closeModal} />
         </Modal>
       </ThemeProvider>
-    </>
+    </QueryClientProvider>
   );
 }
