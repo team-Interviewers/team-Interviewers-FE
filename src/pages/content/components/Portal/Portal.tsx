ca7754b32a0e10fe51de46366758c0e5c6310090
ww8007 <wshmin1234@gmail.com>
2023-09-28 03:44:45 +0900

    fix: 전역으로 설정된 시간 기준으로 trigger 되도록 수정

--- src/pages/content/components/Portal/Portal.tsx
+++ src/pages/content/components/Portal/Portal.tsx
@@ -10,18 +10,13 @@ import { INTERVAL } from '@root/src/constants';
 
 export default function Portal() {
   const queryClient = new QueryClient();
-  const SETTING_TIME = 3;
-  const { Modal, isOpen, closeModal } = useTrigger({ time: SETTING_TIME });
 
-  const [interval, setIntervalState] = useState(
-    storageController.getPortalIntervalTime() || INTERVAL.DEFAULT
+  const [intervalTime, setIntervalState] = useState<number>(
+    () => storageController.getPortalIntervalTime() || INTERVAL.DEFAULT
   );
-
-  useEffect(() => {
-    const timer = setInterval(openModal, interval * 60 * 1000);
-
-    return () => clearInterval(timer);
-  }, [interval]);
+  const { Modal, isOpen, closeModal } = useTrigger({
+    time: intervalTime,
+  });
 
   useEffect(() => {
     const messageListener = (message: any) => {
@@ -30,14 +25,12 @@ export default function Portal() {
         storageController.setPortalIntervalTime(message.interval);
       }
     };
-
     chrome.runtime.onMessage.addListener(messageListener);
 
     return () => {
       chrome.runtime.onMessage.removeListener(messageListener);
     };
   }, []);
-  
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={darkTheme}>
