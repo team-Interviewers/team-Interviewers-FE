ba2437303c837155005121f7bf61a7ba4af9e4e4
ww8007 <wshmin1234@gmail.com>
2023-09-28 03:42:43 +0900

    feat: 전역 문제 하나만 뜨도록 로직 추가

--- src/pages/content/hooks/useTrigger.ts
+++ src/pages/content/hooks/useTrigger.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import useTimer from './useTimer';
 import { useModal } from './useModal';
 import useLife from './useLife';
+import { storageController } from '@root/src/modules/StoreController';
 
 interface TriggerProps {
   time?: number;
@@ -9,7 +10,8 @@ interface TriggerProps {
 
 const useTrigger = ({ time = 60 * 60 }: TriggerProps) => {
   const { isOpen, Modal, openModal, closeModal: _closeModal } = useModal();
-  const { start, reset, isActive } = useTimer(time);
+  const [globalTrigger, setGlobalTrigger] = useState(false);
+  const { start, reset, isActive, isStop, pause } = useTimer(time);
 
   const [isUserOff, setIsUserOff] = useState(false);
   const { lifeCount } = useLife();
@@ -17,18 +19,47 @@ const useTrigger = ({ time = 60 * 60 }: TriggerProps) => {
   // 최초에 사용자가 브라우저를 열면 타이머 시작
   useEffect(() => {
     if (lifeCount === 0) return;
+    if (isStop) return;
     if (!isActive && !isOpen) {
       start();
     }
-  }, [isActive, isOpen, lifeCount]);
+  }, [isActive, isOpen, lifeCount, isStop]);
 
   // 시간이 다 되면 모달 열기
   useEffect(() => {
     if (lifeCount === 0) return;
+    if (isStop) return;
     if (!isActive && !isOpen && !isUserOff) {
       openModal();
     }
-  }, [isActive, isOpen, isUserOff, lifeCount]);
+  }, [isActive, isOpen, isUserOff, lifeCount, isStop]);
+
+  useEffect(() => {
+    if (isOpen) {
+      (async () => {
+        const globalTrigger = await storageController.getTriggerStatus();
+        if (!globalTrigger) {
+          setGlobalTrigger(true);
+          storageController.setTriggerStatus(true);
+        }
+      })();
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    (async () => {
+      const globalTrigger = await storageController.getTriggerStatus();
+      if (globalTrigger) {
+        setTimeout(() => {
+          pause();
+        }, 500);
+        window.addEventListener('beforeunload', () => {
+          storageController.setTriggerStatus(false);
+        });
+        return;
+      }
+    })();
+  }, []);
 
   // 사용자가 모달을 닫으면 타이머 초기화
   useEffect(() => {
@@ -42,11 +73,14 @@ const useTrigger = ({ time = 60 * 60 }: TriggerProps) => {
   const closeModal = () => {
     _closeModal();
     setIsUserOff(true);
+    reset();
+    storageController.setTriggerStatus(false);
+    setGlobalTrigger(false);
   };
 
   return {
     Modal,
-    isOpen,
+    isOpen: globalTrigger,
     closeModal,
     openModal,
   };
