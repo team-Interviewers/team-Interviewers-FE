ff11c3ccc177582b8ff90e799c16a0bd0a79c93f
ww8007 <wshmin1234@gmail.com>
2023-10-03 05:28:00 +0900

    refactor: useReducer를 이용한 trigger 코드 관리 및 life 상태관리 내부로 이전

--- src/pages/content/hooks/useTrigger.ts
+++ src/pages/content/hooks/useTrigger.ts
@@ -1,88 +1,116 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import useTimer from './useTimer';
 import { useModal } from './useModal';
-import useLife from './useLife';
 import { storageController } from '@root/src/modules/StoreController';
 
 interface TriggerProps {
   time?: number;
 }
 
-const useTrigger = ({ time = 60 * 60 }: TriggerProps) => {
-  const { isOpen, Modal, openModal, closeModal: _closeModal } = useModal();
-  const [globalTrigger, setGlobalTrigger] = useState(false);
-  const { start, reset, isActive, isStop, pause } = useTimer(time);
+interface TriggerState {
+  lifeCount: number;
+  isStop: boolean;
+  isActive: boolean;
+}
 
-  const [isUserOff, setIsUserOff] = useState(false);
-  const { lifeCount } = useLife();
+type TriggerAction =
+  | { type: 'OPEN' }
+  | { type: 'CLOSE' }
+  | { type: 'SET_GLOBAL_TRIGGER'; action: boolean }
+  | { type: 'SET_LIFE_COUNT'; action: number };
 
-  // 최초에 사용자가 브라우저를 열면 타이머 시작
+const triggerReducer = (state: TriggerState, action: TriggerAction) => {
+  switch (action.type) {
+    case 'OPEN':
+      return { ...state, isActive: true };
+    case 'CLOSE':
+      return { ...state, isActive: false };
+    case 'SET_GLOBAL_TRIGGER':
+      return { ...state, isActive: action.action };
+    case 'SET_LIFE_COUNT':
+      return { ...state, lifeCount: action.action };
+    default:
+      throw new Error('Unhandled action');
+  }
+};
+
+const useTrigger = ({ time }: TriggerProps) => {
+  const {
+    isOpen,
+    Modal,
+    openModal: _openModal,
+    closeModal: _closeModal,
+  } = useModal();
+  const { start, reset, pause, isActive } = useTimer(time);
+
+  const [state, dispatch] = useReducer(triggerReducer, {
+    lifeCount: 3,
+    isStop: false,
+    isActive: false,
+  });
+
+  // 최초 타이머 시작
   useEffect(() => {
-    if (lifeCount === 0) return;
-    if (isStop) return;
-    if (!isActive && !isOpen) {
-      start();
-    }
-  }, [isActive, isOpen, lifeCount, isStop]);
+    start();
+  }, []);
 
-  // 시간이 다 되면 모달 열기
-  useEffect(() => {
-    if (lifeCount === 0) return;
-    if (isStop) return;
-    if (!isActive && !isOpen && !isUserOff) {
-      openModal();
-    }
-  }, [isActive, isOpen, isUserOff, lifeCount, isStop]);
-
-  useEffect(() => {
-    if (isOpen) {
-      (async () => {
-        const globalTrigger = await storageController.getTriggerStatus();
-        if (!globalTrigger) {
-          setGlobalTrigger(true);
-          storageController.setTriggerStatus(true);
-        }
-      })();
-    }
-  }, [isOpen]);
-
+  // 1. 라이프 카운트 초기화
   useEffect(() => {
     (async () => {
-      const globalTrigger = await storageController.getTriggerStatus();
-      if (globalTrigger) {
-        setTimeout(() => {
-          pause();
-        }, 500);
-        window.addEventListener('beforeunload', () => {
-          storageController.setTriggerStatus(false);
-        });
-        return;
-      }
+      const lifeCount = await storageController.getLifeCount();
+      dispatch({ type: 'SET_LIFE_COUNT', action: lifeCount });
     })();
   }, []);
 
-  // 사용자가 모달을 닫으면 타이머 초기화
-  useEffect(() => {
-    if (lifeCount === 0) return;
-    if (!isOpen) {
-      reset();
-      setIsUserOff(false);
+  // 2. 모달 열림
+  const openModal = async () => {
+    const globalTrigger = await storageController.getTriggerStatus();
+    if (state.lifeCount === 0) return;
+    if (!globalTrigger && !state.isActive) {
+      dispatch({ type: 'OPEN' });
+      await storageController.setTriggerStatus(true);
+      dispatch({ type: 'SET_GLOBAL_TRIGGER', action: true });
+      // NOTE : 창이 닫힐 때 트리거 상태를 false로 변경
+      window.onbeforeunload = async () => {
+        await storageController.setTriggerStatus(false);
+      };
+      _openModal();
+    } else {
+      pause();
     }
-  }, [isOpen, lifeCount]);
+  };
 
-  const closeModal = () => {
-    _closeModal();
-    setIsUserOff(true);
+  // 2-1. 모달 열림
+  useEffect(() => {
+    if (!isActive) {
+      openModal();
+    }
+  }, [isActive]);
+
+  // 3. 모달 닫힘
+  const closeModal = async () => {
+    dispatch({ type: 'CLOSE' });
+    dispatch({ type: 'SET_GLOBAL_TRIGGER', action: false });
+    await storageController.setTriggerStatus(false);
     reset();
-    storageController.setTriggerStatus(false);
-    setGlobalTrigger(false);
+    _closeModal();
+  };
+
+  // 4. 라이프 차감
+  const decreaseLifeCount = () => {
+    const lifeCount = state.lifeCount;
+    if (lifeCount === 0) return;
+    dispatch({ type: 'SET_LIFE_COUNT', action: lifeCount - 1 });
+    storageController.setLifeCount(lifeCount - 1);
   };
 
   return {
     Modal,
-    isOpen: globalTrigger,
+    isOpen,
     closeModal,
     openModal,
+    lifeCount: state.lifeCount,
+    decreaseLifeCount,
   };
 };
 
