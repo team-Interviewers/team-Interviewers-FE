9164e37730f6abb5af2c173eac4ef0f7d08727e8
ww8007 <wshmin1234@gmail.com>
2023-10-03 05:28:12 +0900

    feat: 임시 리셋 버튼 추가

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,15 +1,21 @@
 import '@pages/popup/Popup.css';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import { Interval, Tags } from '@root/src/pages/popup/components';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import Button from '../content/components/Button';
+import { storageController } from '@root/src/modules/StoreController';
+import { lightTheme } from '../content/utils/theme';
 
 const Popup = () => {
   return (
     <div className="App">
-      <Wrapper>
-        <Tags />
-        <Interval />
-      </Wrapper>
+      <ThemeProvider theme={lightTheme}>
+        <Wrapper>
+          <Button onClick={() => storageController.resetStorage()}>리셋</Button>
+          <Tags />
+          <Interval />
+        </Wrapper>
+      </ThemeProvider>
     </div>
   );
 };
